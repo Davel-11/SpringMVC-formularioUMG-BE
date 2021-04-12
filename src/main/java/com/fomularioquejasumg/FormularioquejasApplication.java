704eddd917380647687630d8bdb1e6eaf231acda
davelaltan@gmail.com <davelaltan@gmail.com>
2021-04-11 22:46:51 -0600

    formulario quejas umg BackEnd

---
+++ src/main/java/com/fomularioquejasumg/FormularioquejasApplication.java
@@ -0,0 +1,13 @@
+package com.fomularioquejasumg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularioquejasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularioquejasApplication.class, args);
+	}
+
+}
